aed05333c153baa9d11212a66b2e64b1b38dc35b
Shyam Sunder Verma <shyam@readybytes.in>
2013-12-29 14:57:08 +0530

    Sample input file.

---
+++ input.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+// always fails
+"readybytes.net/super" => "www.readybytes.net/crazy.html",
+
+// home page should be at 
+"jpayplans.com/index.php" => "www.readybytes.net/payplans.html",
+"www.jpayplans.com/index.php" => "www.readybytes.net/payplans.html",
+"www.jpayplans.com" => "www.readybytes.net/payplans.html",
+"www.jpayplans.com/" => "www.readybytes.net/payplans.html",
+
+// home page should be at 
+"readybytes.net" => "www.readybytes.net",
+"readybytes.net/" => "www.readybytes.net",
+"readybytes.net/index.php" => "www.readybytes.net",
+"www.readybytes.net/index.php" => "www.readybytes.net",
+"www.readybytes.net" => "www.readybytes.net",
+"www.readybytes.net/" => "www.readybytes.net",
+);
